a7cb804e4661111fd813422de146f12bf7222694
José Eduardo <37594822+JoseEduardoO@users.noreply.github.com>
2021-02-16 10:40:50 -0300

    Add files via upload

---
+++ silobytes-master/src/main/java/com/MenuController.java
@@ -0,0 +1,23 @@
+
+package com;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author joser
+ */
+public class MenuController {
+
+    
+    @FXML
+    private void cadastrarUsuario() throws IOException{
+        App.setRoot("novoUsuario");
+    }
+    
+    @FXML
+    private void sair(){
+        System.exit(0);
+    }
+}

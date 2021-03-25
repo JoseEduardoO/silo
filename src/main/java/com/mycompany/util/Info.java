3a15ebab6219cc9c15c2dbf5e109755ed4457776
José Eduardo <37594822+JoseEduardoO@users.noreply.github.com>
2021-03-25 13:30:43 -0300

    Delete Info.java

--- src/main/java/com/mycompany/util/Info.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.util;
-
-public class Info {
-    public static final String ARQUIVO_USUARIO = "C:\\silo\\usuario.dat";
-    public static final String ARQUIVO_PRODUTOR = "C:\\silo\\produtor.dat";
-    public static final String ARQUIVO_SILO = "C:\\silo\\silo.dat";
-    public static final String ARQUIVO_ALUGUEL = "C:\\silo\\aluguel.dat";
-}
